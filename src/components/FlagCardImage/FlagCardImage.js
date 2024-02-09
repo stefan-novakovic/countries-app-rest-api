aa152f9a8bf554c3625524c19cea13fe52235862
Stefan Novakovic <snovakovic3@gmail.com>
2024-02-09 13:52:50 +0100

    Seventeenth commit

--- src/components/FlagCardImage/FlagCardImage.js
+++ src/components/FlagCardImage/FlagCardImage.js
@@ -1,10 +1,14 @@
 import "./FlagCardImage.css";
 import useWindowSize from "../../hooks/useWindowResize";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-// import "react-lazy-load-image-component/src/effects/blur.css";
+import placeholderImageLight from "../../images/loading-light-mode.gif";
+import placeholderImageDark from "../../images/loading-dark-mode.gif";
+import { useContext } from "react";
+import DataContext from "../context/DataContext";
 
 const FlagCardImage = ({ country }) => {
   const { width } = useWindowSize();
+  const { darkMode } = useContext(DataContext);
   return (
     <div className="card__img-wrapper" style={{ background: country.flag }}>
       <LazyLoadImage
@@ -16,7 +20,7 @@ const FlagCardImage = ({ country }) => {
             ? country.flags.svg
             : country.flags.png
         }
-        placeholderSrc="https://idejupr.lt/img/0e86394b844d5a56904444f7a7b38c93.gif"
+        placeholderSrc={darkMode ? placeholderImageDark : placeholderImageLight}
       />
     </div>
   );
