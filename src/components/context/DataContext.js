4a7bd2230465829a7664133428d842dc9c4149db
Stefan Novakovic <snovakovic3@gmail.com>
2024-02-12 06:21:03 +0100

    26th commit

--- src/components/context/DataContext.js
+++ src/components/context/DataContext.js
@@ -1,10 +1,13 @@
-import { createContext, useCallback } from "react";
+import { createContext } from "react";
 import { useState, useEffect, useRef } from "react";
 
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
+  const baseUrl = "https://restcountries.com/v3.1";
+  const [allCountries, setAllCountries] = useState([]);
   const [countries, setCountries] = useState([]);
+  const [allFilteredCountries, setAllFilteredCountries] = useState([]);
   const [fetchError, setFetchError] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [search, setSearch] = useState("");
@@ -19,113 +22,133 @@ export const DataProvider = ({ children }) => {
     localStorage.setItem("dark-mode", JSON.stringify(darkMode));
   }, [darkMode]);
 
-  const fetchData = useCallback(async () => {
-    try {
-      const response = await fetch(
-        "https://restcountries.com/v3.1/all?fields=flags,name,region,population,capital"
-      );
-      if (!response.ok) throw Error("Please reload the app");
-      const data = await response.json();
-      const dataSortedByCountryNames = data.sort((a, b) =>
-        a.name.common.localeCompare(b.name.common)
-      );
-
-      setCountries(dataSortedByCountryNames.slice(0, (pageNumber + 1) * 20));
-      setFetchError("");
-    } catch (err) {
-      setFetchError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [pageNumber]);
-
-  const fetchFilteredCountries = useCallback(async () => {
-    if (filter) {
+  useEffect(() => {
+    const fetchData = async () => {
       try {
         const response = await fetch(
-          `https://restcountries.com/v3.1/region/${filter}?fields=flags,name,region,population,capital`
+          `${baseUrl}/all?fields=flags,name,region,population,capital`
         );
-        if (!response.ok) throw Error("Empty");
+        if (!response.ok) throw Error("Please reload the app");
         const data = await response.json();
         const dataSortedByCountryNames = data.sort((a, b) =>
           a.name.common.localeCompare(b.name.common)
         );
 
-        setCountries(dataSortedByCountryNames.slice(0, (pageNumber + 1) * 20));
+        setAllCountries(dataSortedByCountryNames);
+        setCountries(dataSortedByCountryNames.slice(0, 20));
         setFetchError("");
       } catch (err) {
         setFetchError(err.message);
       } finally {
         setIsLoading(false);
       }
-    }
-  }, [filter, pageNumber]);
+    };
 
-  const fetchSearchedCountries = useCallback(async () => {
-    try {
-      if (filter.length === 0) {
+    const fetchFilteredData = async () => {
+      try {
         const response = await fetch(
-          `https://restcountries.com/v3.1/name/${search.toLowerCase().trim()}`
+          `${baseUrl}/region/${filter}?fields=flags,name,region,population,capital`
         );
-        if (!response.ok) throw Error("Empty");
+        if (!response.ok) throw Error("Please reload the app");
         const data = await response.json();
         const dataSortedByCountryNames = data.sort((a, b) =>
           a.name.common.localeCompare(b.name.common)
         );
 
-        setCountries(dataSortedByCountryNames.slice(0, (pageNumber + 1) * 20));
+        setPageNumber(0);
+        setAllFilteredCountries(dataSortedByCountryNames);
+        setCountries(dataSortedByCountryNames.slice(0, 20));
         setFetchError("");
-      } else {
-        const response = await fetch(
-          `https://restcountries.com/v3.1/region/${filter}?fields=flags,name,region,population,capital`
-        );
-        if (!response.ok) throw Error("Empty");
-        const data = await response.json();
-        const filterData = data.filter((item) =>
-          item.name.common.toLowerCase().includes(search.toLowerCase().trim())
-        );
-
-        if (filterData.length === 0) {
-          setFetchError("Empty");
-        } else {
-          setFetchError("");
-        }
-        const dataSortedByCountryNames = filterData.sort((a, b) =>
-          a.name.common.localeCompare(b.name.common)
-        );
-
-        setCountries(dataSortedByCountryNames.slice(0, (pageNumber + 1) * 20));
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
       }
-    } catch (err) {
-      setFetchError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [filter, search, pageNumber]);
+    };
 
-  // INITIAL LOAD AND SEARCH
-  useEffect(() => {
-    if (search.length > 0) {
-      fetchSearchedCountries();
+    if (filter.length > 0) {
+      fetchFilteredData();
     } else {
-      if (filter.length === 0) {
-        fetchData();
-      } else {
-        fetchFilteredCountries();
-      }
+      fetchData();
     }
-  }, [
-    search,
-    filter,
-    fetchSearchedCountries,
-    fetchFilteredCountries,
-    fetchData,
-  ]);
+  }, [filter]);
 
-  // FILTER
   useEffect(() => {
-    fetchFilteredCountries();
-  }, [fetchFilteredCountries]);
+    setCountries(allCountries.slice(0 * 20, (pageNumber + 1) * 20));
+  }, [allCountries, pageNumber]);
+
+  useEffect(() => {
+    if (filter.length > 0)
+      setCountries(allFilteredCountries.slice(0 * 20, (pageNumber + 1) * 20));
+  }, [filter, allFilteredCountries, pageNumber]);
+
+  // const fetchFilteredCountries = useCallback(async () => {
+
+  // const fetchSearchedCountries = useCallback(async () => {
+  //   try {
+  //     if (filter.length === 0) {
+  //       const response = await fetch(
+  //         `${baseUrl}/name/${search.toLowerCase().trim()}`
+  //       );
+  //       if (!response.ok) throw Error("Empty");
+  //       const data = await response.json();
+  //       const dataSortedByCountryNames = data.sort((a, b) =>
+  //         a.name.common.localeCompare(b.name.common)
+  //       );
+
+  //       setCountries(dataSortedByCountryNames);
+  //       setFetchError("");
+  //     } else {
+  //       const response = await fetch(
+  //         `${baseUrl}/region/${filter}?fields=flags,name,region,population,capital`
+  //       );
+  //       if (!response.ok) throw Error("Empty");
+  //       const data = await response.json();
+  //       const filterData = data.filter((item) =>
+  //         item.name.common.toLowerCase().includes(search.toLowerCase().trim())
+  //       );
+
+  //       if (filterData.length === 0) {
+  //         setFetchError("Empty");
+  //       } else {
+  //         setFetchError("");
+  //       }
+  //       const dataSortedByCountryNames = filterData.sort((a, b) =>
+  //         a.name.common.localeCompare(b.name.common)
+  //       );
+
+  //       setCountries(dataSortedByCountryNames);
+  //     }
+  //   } catch (err) {
+  //     setFetchError(err.message);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // }, [filter, search]);
+
+  // // INITIAL LOAD AND SEARCH
+  // useEffect(() => {
+  //   if (search.length > 0) {
+  //     fetchSearchedCountries();
+  //   } else {
+  //     if (filter.length === 0) {
+  //       // AAAAAAAAAAAAAAAAAAAAA
+  //     } else {
+  //       fetchFilteredCountries();
+  //     }
+  //   }
+  // }, [
+  //   search,
+  //   filter,
+  //   fetchSearchedCountries,
+  //   fetchFilteredCountries,
+  //   allCountries,
+  // ]);
+
+  // // FILTER
+  // useEffect(() => {
+  //   fetchFilteredCountries();
+  // }, [fetchFilteredCountries]);
 
   // INFINITE SCROLL AND PAGINATION
   useEffect(() => {
